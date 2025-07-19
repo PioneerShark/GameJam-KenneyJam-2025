6b7bdf3a7bf5199bb9df690f3212ea3282ff905c
Adewuyi <72622452+PioneerShark@users.noreply.github.com>
2025-07-19 17:35:09 +0100

    room spawns

--- Assets/Reusable scripts/Framework/Components/MovementComponent.cs
+++ Assets/Reusable scripts/Framework/Components/MovementComponent.cs
@@ -24,14 +24,14 @@ public class MovementComponent : MonoBehaviour
     float skinWidth = 0.06f;
     int collideIterations = 5;
     public Collider col;
-    private LayerMask collisionMask;
+    public LayerMask collisionMask;
 
     [HideInInspector]
     public Vector2 moveVector;
 
     private void Awake()
     {
-        collisionMask = LayerMask.GetMask("Wall");
+        //collisionMask = LayerMask.GetMask("Wall");
         
         Collider[] colliders = gameObject.GetComponents<Collider>();
         foreach (Collider collider in colliders)
